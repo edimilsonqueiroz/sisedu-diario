5aa3a32e56f307ba02e4645df0f0201a7353259d
edimilsonqueiroz <you@example.com>
2024-08-11 12:30:40 -0300

    pronto

---
+++ resources/views/livewire/login.blade.php
@@ -0,0 +1,16 @@
+<div class="w-full h-full">
+   <div class="w-full flex h-full">
+        <div class="w-[60%] md:flex hidden h-full bg-cyan-600">
+            ....
+        </div>
+        <div class="flex-1 flex items-center justify-center">
+            <form class="flex flex-col w-full px-5">
+                <label>Login</label>
+                <input class="border-2 mb-5 p-2 outline-none rounded-md border-gray-300" type="email" required>
+                <label>Senha</label>
+                <input class="border-2 p-2 outline-none rounded-md border-gray-300" type="password" required>
+                <button class="bg-cyan-500 mt-8 py-3 text-white rounded-md border-0">Entrar</button>
+            </form>
+        </div>
+   </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\LoginController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', LoginController::class);
